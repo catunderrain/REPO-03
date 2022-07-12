508ab322274b2a7a0b7eaa5c2c8fb7ef9515cb71
Noire <thaitoanvn1349@gmail.com>
2022-07-12 13:40:25 +0700

    ok

--- PYTHON/ProjectsB/PHOTOS/tempCodeRunnerFile.py
+++ PYTHON/ProjectsB/PHOTOS/tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
-for i in range(len(a)):
-#     a[i] = numpy.int8(50)
+    # if len(a) % 2 == 1:
+    #     a = a[:len(a) - 1]
+    #     b = b[:len(b) - 1]
+    #     c = c[:len(c) - 1]
--- a.py
+++ a.py
@@ -1,9 +1,6 @@
-from webbot import Browser
-web = Browser()
-web.go_to('google.com')
-web.click('Sign in')
-web.type('mymail@gmail.com', into='Email')
-web.click('NEXT', tag='span')
-web.type('mypassword', into='Password',
-         id='passwordFieldId')  # specific selection
-web.click('NEXT', tag='span')  # you are logged in ^_^
+a = [1, 2, 3, 4, 5]
+print(len(a))
+if len(a) % 2 == 1:
+    a = a[:len(a) - 2]
+(print(len(a)))
+print(a)
